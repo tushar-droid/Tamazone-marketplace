b6cddabe5c293a92d7cbcd2325800054c721ffaa
tushar-droid <tusharsandhu3022@gmail.com>
2024-12-02 17:22:43 -0800

    set up home screen about to add chakra UI

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,23 @@
 import Navbar from "./Navbar";
-
+import { Link } from "react-router-dom";
+import "./Styles/homeStyles.css";
 const Home = () => {
   return (
     <>
       <Navbar />
-      Home
+      <div className="hero">
+        <div className="left">
+          <img src="../assets/home-screen.jpg" alt="home-screen-image" />
+        </div>
+        <div className="right">
+          <h1 className="hero-title">
+            Discover Quality and Innovation in Every Product
+          </h1>
+          <Link to="/products">
+            <button className="call-to-action">Shop Now</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
